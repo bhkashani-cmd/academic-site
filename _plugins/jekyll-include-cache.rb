b7c2b0775829c90ce2c8a2792070f85ce49b66b7
bhkashani-cmd <bhkashani@gmail.com>
2026-08-02 16:23:41 +0330

    Update jekyll-include-cache.rb

--- _plugins/jekyll-include-cache.rb
+++ _plugins/jekyll-include-cache.rb
@@ -1,1 +1,13 @@
+module Jekyll
+  class IncludeCacheTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      super
+    end
 
+    def render(context)
+      ""
+    end
+  end
+end
+
+Liquid::Template.register_tag('include_cached', Jekyll::IncludeCacheTag)
